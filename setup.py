7d4652a5f3a502f56784e667406110b610bfa6c9
Zachary Johnson <phyzachs@gmail.com>
2025-02-10 16:01:36 -0500

    udpate installation

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='ConfigurationGeneration',
+    name='CorrelatedConfigurations',
     version='0.1.0',
     packages=find_packages(),  # Automatically find packages in the directory
     install_requires=['numpy', 'scipy'],
     author='Zachary A. Johnson',
-    description='A description of my package',
+    description='Generates configurations of particles for use in initializing molecular dynamics simulations.',
     url='https://github.com/ZachAJohnson/CorrelatedConfigurations',
 )
