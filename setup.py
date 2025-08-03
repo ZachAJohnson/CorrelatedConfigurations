319bfda732a8c09c51b22c7b33bd6d5f519a7c12
ZachAJohnson <phyzachs@gmail.com>
2025-08-03 17:19:04 -0400

    leaving msu, backing up

---
+++ setup.py
@@ -0,0 +1,12 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='md_init',
+    version='0.1.0',
+    packages=find_packages(),  # Automatically find packages in the directory
+    install_requires=['numpy', 'scipy'],
+    author='Zachary A. Johnson',
+    description='Generates configurations of particles for use in initializing molecular dynamics simulations.',
+    url='https://github.com/ZachAJohnson/CorrelatedConfigurations',
+)
